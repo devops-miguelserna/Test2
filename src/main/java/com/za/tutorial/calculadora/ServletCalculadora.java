1d42217c5c0f9151731345a6e28c4d0a85ac96d6
Luis Miguel Serna Acevedo <miguel8810@gmail.com>
2017-09-01 12:22:59 -0500

    asd

--- src/main/java/com/za/tutorial/calculadora/ServletCalculadora.java
+++ src/main/java/com/za/tutorial/calculadora/ServletCalculadora.java
@@ -8,20 +8,20 @@ import javax.servlet.http.HttpServletResponse;
 
 
 
-public class ServletCalculadora extends HttpServlet { //metodo para crear el Servlet
-	private static final long serialVersionUID = 1L; //metodo para crear el Servlet
-    public ServletCalculadora() { //metodo para crear el Servlet
-        super(); //metodo para crear el Servlet
-    } //metodo para crear el Servlet
+public class ServletCalculadora extends HttpServlet { 
+	private static final long serialVersionUID = 1L; 
+    public ServletCalculadora() { 
+        super(); 
+    } 
 
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { //metodo para crear el Servlet
-		Calculadora calcu = new Calculadora(); //metodo para crear el Servlet
-		calcu.setA(Integer.parseInt(request.getParameter("a"))); //metodo para crear el Servlet
-		calcu.setB(Integer.parseInt(request.getParameter("b"))); //metodo para crear el Servlet
-		calcu.doSomething(); //metodo para crear el Servlet
-		request.setAttribute("calcu", calcu); //metodo para crear el Servlet
-		getServletContext().getRequestDispatcher("/index2.jsp").forward(request, response); //metodo para crear el Servlet
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
+		Calculadora calcu = new Calculadora(); 
+		calcu.setA(Integer.parseInt(request.getParameter("a"))); 
+		calcu.setB(Integer.parseInt(request.getParameter("b"))); 
+		calcu.doSomething(); 
+		request.setAttribute("calcu", calcu); 
+		getServletContext().getRequestDispatcher("/index2.jsp").forward(request, response); 
 		
 	}
 	
